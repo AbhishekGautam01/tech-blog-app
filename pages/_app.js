0d537928f5093bdd501bda44704c72d743343566
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-18 22:17:58 +0530

    Initial Setup

---
+++ pages/_app.js
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({ Component, pageProps }) => <Component {...pageProps} />;
