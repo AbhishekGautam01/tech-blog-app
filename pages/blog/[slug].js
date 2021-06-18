737e296828890303633453450d3aef5a74ef1afa
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-19 03:47:51 +0530

    Adding Blog Detail Basic Skeleton

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,10 +1,24 @@
 import PageLayout from 'components/PageLayout';
+import BlogHeader from 'components/BlogHeader';
 import { getBlogBySlug, getAllBlogs } from 'lib/api';
+import { Row, Col } from 'react-bootstrap';
 
 const BlogDetail = ({ blog }) => {
   return (
-    <PageLayout>
-      <h1>Hello {blog?.slug}</h1>
+    <PageLayout className="blog-detail-page">
+      <Row>
+        <Col md={{ span: 10, offset: 1 }}>
+          <BlogHeader
+            title={blog.title}
+            subtitle={blog.subtitle}
+            coverImage={blog.coverImage}
+            author={blog.author}
+            date={blog.date}
+          />
+          <hr />
+          {/* Blog Content Here */}
+        </Col>
+      </Row>
     </PageLayout>
   );
 };
