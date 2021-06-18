cad87ba29eb31eed9c19ef3a8cfdf90df5624f71
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-19 02:47:40 +0530

    Added to display author details in post

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,29 +1,31 @@
 import { Card } from 'react-bootstrap';
 import { CardSubtitle } from 'react-bootstrap/Card';
-const CardItem = ({ title, subtitle }) => {
+const CardItem = ({
+  title,
+  subtitle,
+  date,
+  coverImage,
+  author,
+  authorImage,
+}) => {
   return (
     <Card className={`fj-card`}>
       <div className="card-body-wrapper">
         <Card.Header className="d-flex flex-row">
           <img
-            src={'https://via.placeholder.com/150'}
+            src={authorImage}
             className="rounded-circle mr-3"
             height="50px"
             width="50px"
             alt="avatar"
           />
           <div>
-            <Card.Title className="font-weight-bold mb-1">
-              Placeholder Author
-            </Card.Title>
-            <Card.Text className="card-date">Placeholder Date</Card.Text>
+            <Card.Title className="font-weight-bold mb-1">{author}</Card.Title>
+            <Card.Text className="card-date">{date}</Card.Text>
           </div>
         </Card.Header>
         <div className="view overlay">
-          <Card.Img
-            src="https://via.placeholder.com/250"
-            alt="Card image cap"
-          />
+          <Card.Img src={coverImage} alt="Card image cap" />
         </div>
         <Card.Body>
           <Card.Title className="card-main-title">{title}</Card.Title>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,16 @@ export default function Home({ blogs }) {
         </Col> */}
         {blogs.map((blog) => (
           <Col key={blog.slug} md="4">
-            <CardItem title={blog.title} subtitle={blog.subtitle} />
+            <CardItem
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              coverImage={blog.coverImage}
+              author={blog.author?.name || 'Anonymous'}
+              authorImage={
+                blog.author?.avatar || 'https://via.placeholder.com/150'
+              }
+            />
           </Col>
         ))}
       </Row>
