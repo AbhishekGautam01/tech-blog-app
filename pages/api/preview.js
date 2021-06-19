699da2af9b303bddd4183a0d0a99c677d7e15909
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-19 18:44:27 +0530

    Added Preview API route

---
+++ pages/api/preview.js
@@ -0,0 +1,12 @@
+import { getBlogBySlug } from 'lib/api';
+
+export default async function enablePreview(req, res) {
+  if (req.query.secret !== process.env.SANITY_PREVIEW_SECRET || !req.query.slug)
+    return res.status(401).json({ message: 'Invalid Token' });
+
+  const blog = await getBlogBySlug(req.query.slug);
+  if (!blog) return res.status(401).json({ message: 'Invalid Slug' });
+  res.setPreviewData({ message: 'Hello World' });
+  res.writeHead(307, { Location: `/blog/${blog.slug}` });
+  res.end();
+}
