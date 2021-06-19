9cefa65ef1d9e507fd97af38d57c3bafafb9977b
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-19 10:45:17 +0530

    Adding basic list filtering

--- pages/index.js
+++ pages/index.js
@@ -3,34 +3,48 @@ import PageLayout from 'components/PageLayout';
 import AuthorInfo from 'components/AuthorIntro';
 import CardItem from 'components/CardItem';
 import CardListItem from 'components/CardListItem';
+import FilteringMenu from 'components/FilteringMenu';
 import { getAllBlogs } from 'lib/api';
+import { useState } from 'react';
 export default function Home({ blogs }) {
+  const [filter, setFilter] = useState({
+    view: { list: false },
+  });
   return (
     <PageLayout>
       <AuthorInfo />
+      <FilteringMenu
+        filter={filter}
+        onChange={(option, value) => {
+          setFilter({ ...filter, [option]: value });
+        }}
+      />
       <hr />
       <Row className="mb-5">
-        {/* <Col md="10">
-          <CardListItem />
-        </Col> */}
-        {blogs.map((blog) => (
-          <Col key={blog.slug} md="4">
-            <CardItem
-              title={blog.title}
-              subtitle={blog.subtitle}
-              date={blog.date}
-              coverImage={blog.coverImage}
-              author={blog.author?.name || 'Anonymous'}
-              authorImage={
-                blog.author?.avatar || 'https://via.placeholder.com/150'
-              }
-              link={{
-                href: '/blog/[slug]',
-                as: `/blog/${blog.slug}`,
-              }}
-            />
-          </Col>
-        ))}
+        {blogs.map((blog) =>
+          filter.view.list ? (
+            <Col key={`${blog.slug}-list`} md="10">
+              <CardListItem />
+            </Col>
+          ) : (
+            <Col key={blog.slug} md="4">
+              <CardItem
+                title={blog.title}
+                subtitle={blog.subtitle}
+                date={blog.date}
+                coverImage={blog.coverImage}
+                author={blog.author?.name || 'Anonymous'}
+                authorImage={
+                  blog.author?.avatar || 'https://via.placeholder.com/150'
+                }
+                link={{
+                  href: '/blog/[slug]',
+                  as: `/blog/${blog.slug}`,
+                }}
+              />
+            </Col>
+          )
+        )}
       </Row>
     </PageLayout>
   );
