e3b38bad06e39720d8d9d77c3d1c6ee8a3f418e5
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-06-19 14:01:55 +0530

    Added a basic pagination using swr

---
+++ actions/pagination.js
@@ -0,0 +1,58 @@
+import { useSWRPages } from 'swr';
+import { useGetBlogs } from 'actions';
+import CardItem from 'components/CardItem';
+import CardListItem from 'components/CardListItem';
+import { Col } from 'react-bootstrap';
+
+export const useGetBlogsPages = ({ blogs: initialData, filter }) => {
+  return useSWRPages(
+    'index-page',
+    ({ offset, withSWR }) => {
+      const { data: blogs } = withSWR(useGetBlogs(initialData));
+      console.log(filter);
+      if (!blogs) return 'Loading!';
+      return blogs.map((blog) =>
+        filter.view.list ? (
+          <Col key={`${blog.slug}-list`} md="10">
+            <CardListItem
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              author={blog.author?.name || 'Anonymous'}
+              authorImage={
+                blog.author?.avatar || 'https://via.placeholder.com/150'
+              }
+              link={{
+                href: '/blog/[slug]',
+                as: `/blog/${blog.slug}`,
+              }}
+            />
+          </Col>
+        ) : (
+          <Col key={blog.slug} md="4">
+            <CardItem
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              coverImage={blog.coverImage}
+              author={blog.author?.name || 'Anonymous'}
+              authorImage={
+                blog.author?.avatar || 'https://via.placeholder.com/150'
+              }
+              link={{
+                href: '/blog/[slug]',
+                as: `/blog/${blog.slug}`,
+              }}
+            />
+          </Col>
+        )
+      );
+    },
+    // here you will compute offset that will get passed into previous callback function
+    // SWR: data you will get from 'withSWR' and index is number of current page
+    (SWR, index) => {
+      return 0;
+    },
+    [filter.view.list]
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import { Row, Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import PageLayout from 'components/PageLayout';
 import AuthorInfo from 'components/AuthorIntro';
-import CardItem from 'components/CardItem';
-import CardListItem from 'components/CardListItem';
+import { useGetBlogsPages } from 'actions/pagination';
 import FilteringMenu from 'components/FilteringMenu';
 import { getAllBlogs } from 'lib/api';
 import { useState } from 'react';
-import { useGetBlogs } from 'actions';
 
-export default function Home({ blogs: initialData }) {
+export default function Home({ blogs }) {
   const [filter, setFilter] = useState({
     view: { list: false },
   });
-
-  const { data: blogs, error } = useGetBlogs(initialData);
+  const { pages, isLoadingMore, isReachingEnd, loadMore } = useGetBlogsPages({
+    blogs,
+    filter,
+  });
 
   return (
     <PageLayout>
@@ -25,44 +25,7 @@ export default function Home({ blogs: initialData }) {
         }}
       />
       <hr />
-      <Row className="mb-5">
-        {blogs.map((blog) =>
-          filter.view.list ? (
-            <Col key={`${blog.slug}-list`} md="10">
-              <CardListItem
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                author={blog.author?.name || 'Anonymous'}
-                authorImage={
-                  blog.author?.avatar || 'https://via.placeholder.com/150'
-                }
-                link={{
-                  href: '/blog/[slug]',
-                  as: `/blog/${blog.slug}`,
-                }}
-              />
-            </Col>
-          ) : (
-            <Col key={blog.slug} md="4">
-              <CardItem
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                coverImage={blog.coverImage}
-                author={blog.author?.name || 'Anonymous'}
-                authorImage={
-                  blog.author?.avatar || 'https://via.placeholder.com/150'
-                }
-                link={{
-                  href: '/blog/[slug]',
-                  as: `/blog/${blog.slug}`,
-                }}
-              />
-            </Col>
-          )
-        )}
-      </Row>
+      <Row className="mb-5">{pages}</Row>
     </PageLayout>
   );
 }
